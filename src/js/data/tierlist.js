d57e9600c1cb81d7312d6d1832ce019fb04f64c2
Medicine Melancholy <56356893+MedicineMelancholy0@users.noreply.github.com>
2026-07-12 18:26:35 -0400

    Update tierlist.js

--- src/js/data/tierlist.js
+++ src/js/data/tierlist.js
@@ -1,10 +1,10 @@
 dataSetVersion = "tierlist"; // Change this when creating a new data set version. YYYY-MM-DD format.
 dataSet[dataSetVersion] = {};
-const container = document.getElementByID("EX");
+const container = document.getElementById("EX");
 
 function addCharacter(item) {
   const i = document.createElement("div");
-  i.innerHTML = <img src="src/assets/${item.img}" />;
+  i.innerHTML = `<img src="src/assets/${item.img}" />`;
   
   container.appendChild(i);
 }
